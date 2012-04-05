22a039dc1f266ad1b69ac11c18d6c4f87b8edc3b
= <=>
2012-04-04 23:51:17 -0400

    Removed MemoryDHT from the simulation

--- src/simulation/node/WellBehavedDataProvider.java
+++ src/simulation/node/WellBehavedDataProvider.java
@@ -1,11 +1,18 @@
 package simulation.node;
 
+import java.util.UUID;
+
 import node.DataProvider;
 
 public class WellBehavedDataProvider implements DataProvider {
 
 	@Override
 	public void registerDataRequest(DataRequest dataRequest) {
+	}
+
+	@Override
+	public UUID getId() {
+		return null;
 	}
 
 }
