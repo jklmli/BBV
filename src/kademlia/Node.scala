5520ed01aeebb6cd9b7939a162b0676e676aeb78
Jiawei Li <jiawei.h.li@gmail.com>
2012-04-11 14:45:23 -0500

    cleaned up Node by removing assumed usages

--- src/kademlia/Node.scala
+++ src/kademlia/Node.scala
@@ -1,8 +1,6 @@
 import java.util.UUID
 import util.Random
 
-import scala.collection.mutable.Set
-
 object Node {
   // UUID hashcodes are Ints, which are 32 bits
   private final val buckets = 32
@@ -11,43 +9,18 @@ object Node {
   private final val threads = 3
 }
 
-class Node {
+class Node(val id: UUID = UUID.randomUUID()) {
 
-  val id = UUID.randomUUID()
-
-  // Initially associate 1-10 'files' (represented by hashes) with this Node.
-  val files = Set[UUID]()
-  files ++=
-    (1 until (new Random()).nextInt(10) toList)
-      .map(_ => UUID.randomUUID())
-
-  private val buckets: IndexedSeq[Set[Node]] = IndexedSeq.fill(Node.buckets)(Set())
+  private val buckets = Seq.fill(Node.buckets)(scala.collection.mutable.Set())
 
   def ping(node: Node): Boolean = closestNodes(node.id).exists(_ == node)
-
-  // Finds the _bucketDepth_ closest Nodes
-  private def closestNodes(key: UUID): scala.collection.immutable.Set[Node] = {
-    val closestIndices =
-      buckets
-        .indices
-        .sortBy(x => (x - distanceTo(key)).abs)
-
-    val closestBuckets =
-      closestIndices
-        .map(buckets(_))
-
-    closestBuckets
-      .flatten(set => set)
-      .slice(0, Node.threads)
-      .toSet
-  }
 
   def findNode(key: UUID): Node = {
     new Node()
   }
 
-  def findFile(that: Node, key: UUID): Boolean = {
-    new Boolean()
+  def findFile(key: UUID): Boolean = {
+    true
   }
 
   def send(that: Node, key: UUID) {
@@ -79,6 +52,23 @@ class Node {
     that.unstore(this)
 
     this
+  }
+
+  // Finds the _bucketDepth_ closest Nodes
+  private def closestNodes(key: UUID): Set[Node] = {
+    val closestIndices =
+      buckets
+        .indices
+        .sortBy(x => (x - distanceTo(key)).abs)
+
+    val closestBuckets =
+      closestIndices
+        .map(buckets(_))
+
+    closestBuckets
+      .flatten(set => set)
+      .slice(0, Node.threads)
+      .toSet
   }
 
   private def store(that: Node) {
