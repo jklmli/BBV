9b44b21161208e7970ba994fc28352cef21770e1
Jiawei Li <jiawei.h.li@gmail.com>
2012-04-17 16:56:17 -0500

    scala-izing BankNode into trait Banker

---
+++ src/node/Banker.scala
@@ -0,0 +1,21 @@
+package node
+
+trait Banker {
+  val bank = scala.collection.mutable.Set[CurrencyUnit]()
+
+  def add(currencyUnits: Traversable[CurrencyUnit]) {
+    currencyUnits.foreach(currencyUnit => {
+      assert(!(bank contains currencyUnit))
+      bank += currencyUnit
+    })
+  }
+
+  def remove(currencyUnits: Traversable[CurrencyUnit]) {
+    currencyUnits.foreach(currencyUnit => {
+      assert(bank contains currencyUnit)
+      bank -= currencyUnit
+    })
+  }
+
+  def getCurrencyUnits(nodeId: UUID, ids: Traversable[UUID]): Traversable[CurrencyUnit]
+}
