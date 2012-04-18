e1de012e63e38eb917632b3a37b1a82e8ed687c4
Jiawei Li <jiawei.h.li@gmail.com>
2012-04-17 19:55:23 -0500

    scala-izing data.Data

---
+++ src/data/Data.scala
@@ -0,0 +1,10 @@
+package data
+
+class Data (val bytes: Array[Byte]) {
+  override def equals(that: Any): Boolean = {
+    that.isInstanceOf[Data] && that.hashCode == this.hashCode
+  }
+
+  override def hashCode: Int = bytes.hashCode
+}
+
