0bd6469cc90116fccd8b8ce313262c361d12a6e5
Jiawei Li <jiawei.h.li@gmail.com>
2012-04-22 17:42:02 -0500

    parallel-izing User

--- src/main/scala/node/User.scala
+++ src/main/scala/node/User.scala
@@ -1,16 +1,28 @@
 package main.scala.node
 
 import main.scala.data.Data
+import actors.Actor
 
-trait User {
+trait User extends Actor {
   val files = scala.collection.mutable.Set[Data]()
 
-  def store(file: Data) {
+  def act() {
+    loop {
+      react {
+        case ("store", file: Data) =>
+          store(file)
+        case ("unstore", file: Data) =>
+          unstore(file)
+      }
+    }
+  }
+
+  private def store(file: Data) {
     assert(!(files contains file))
     files += file
   }
 
-  def unstore(file: Data) {
+  private def unstore(file: Data) {
     assert(files contains file)
     files -= file
   }
